0831d596438b18dd54bf80f14b5daf955fc93186
Joepiano <99508912+Joepiano@users.noreply.github.com>
2022-03-11 11:59:10 -1100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -Entry point 
+ * main - Entry point 
  *
  * Return: 0
  */
@@ -9,7 +9,7 @@ int main(void)
 	char c;
 	int i;
 
-	for (i = 0; i < 16; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
 	}
