3a1dfcf7a402efe6aeb12b00af5a479c52b7a45c
Joepiano <99508912+Joepiano@users.noreply.github.com>
2022-03-28 10:58:15 -1100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,13 +1,28 @@
 #include "main.h"
 
 /**
- * set_string - sets the value of a pointer to a char
- * @s: source address
- * @to: target address
- * Return: void
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character
+ * Return: pointer to the first occurrence of the character c
  */
 
-void set_string(char **s, char *to)
+char *_strchr(char *s, char c)
 {
-	*s = to;
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+
+		++s;
+	}
+
+	if (*s == c)
+	{
+		return (s);
+	}
+
+	return (0);
 }
