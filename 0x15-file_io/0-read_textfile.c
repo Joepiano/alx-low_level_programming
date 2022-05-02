b97063d8793f9e2e2ead936938e08584d0b5744a
Joepiano <99508912+Joepiano@users.noreply.github.com>
2022-05-02 06:21:33 -1100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,51 +1,44 @@
 #include "main.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 
 /**
- * read_textfile -  reads a text file and prints it
- * to the POSIX standard output
- * @filename: name of the file
- * @letters: number of letters it should read and print
- *
- * Return: returns the actual number of letters it could read and print
- * 0 if file cannot be opened or read
- * 0 if filename is NULL
- * 0 if write fails or does not write expected amount of bytes
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: type char filename
+ * @letters: type size_t letters
+ * Return: if cannot be oppened or read 0, if is NULL 0, if writes fail 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, checkr, checkw;
-	char *c;
 
-	if (filename == 0)
+	int fd;
+	ssize_t rd, wr;
+	char *buf;
+
+	if (filename == NULL)
 		return (0);
-
-	c = malloc(letters + 1);
-
-	if (c == 0)
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 		return (0);
-
-	fd  = open(filename, O_RDONLY);
-
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (free(c), 0);
-
-	checkr = read(fd, c, letters);
-
-	if (checkr == -1)
-		return (free(c), 0);
-
-	c[letters] = '\0';
-
-	checkw = write(STDOUT_FILENO, c, checkr);
-	if (checkw == -1)
-		return (free(c), 0);
-
-	free(c);
+	{
+		free(buf);
+		return (0);
+	}
+	rd = read(fd, buf, letters);
+	if (rd == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 	close(fd);
-	return (checkw);
+	wr = write(STDOUT_FILENO, buf, rd);
+	if (wr == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	if (wr != rd)
+		return (0);
+	return (rd);
 }
