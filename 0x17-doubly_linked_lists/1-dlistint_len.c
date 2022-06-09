3d2d510227e9661308d6cb39b7d3987e1b330c8b
Joepiano <99508912+Joepiano@users.noreply.github.com>
2022-06-09 07:56:03 -1100

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,24 +1,20 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_dlistint - print elements of doubly linked list
- * @h: linked list
+ * dlistint_len - Counts the number of elements in a linked dlistint_t list.
+ * @h: The head of the dlistint_t list.
  *
- * Return: number of nodes
+ * Return: The number of elements in the dlistint_t list.
  */
-
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
-	size_t nbr_nodes = 0;
-	const dlistint_t *current = h;
+	size_t nodes = 0;
 
-	while (current != NULL)
+	while (h)
 	{
-		printf("%i\n", current->n);
-		current = current->next;
-		nbr_nodes++;
+		nodes++;
+		h = h->next;
 	}
 
-	return (nbr_nodes);
+	return (nodes);
 }
